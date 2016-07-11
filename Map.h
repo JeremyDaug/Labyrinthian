600c56373446486c8e5937f46e25485a823c02bf
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2016-07-11 01:33:51 -0500

    Etc. Etc. Goodnight.

---
+++ Map.h
@@ -0,0 +1,17 @@
+#ifdef MAP_H
+#define MAP_H
+
+#include "QuadTree.h"
+
+class Map
+{
+    // Current Cell
+
+    // Surrounding Cells
+
+    // The Tree itself
+
+    // Functions
+}
+
+#endif // !MAP_H
--- Node.h
+++ Node.h
@@ -1,51 +1,48 @@
 #ifndef NODE_H
 #define NODE_H
 
+struct Tile
+{
+    int x;
+    int y;
 
+    Tile(int nx, int ny) : x(nx), y(ny) {}
+}
+
+template <typename T>
+struct Data
+{
+    Tile pos;
+    T Data;
+
+    Data(Tile position = Tile(), T* data = nullptr) : pos(position), Data(data) {}
+}
+
+template <typename T>
 class Node
 {
-    public:
-        /** Default constructor */
-        Node();
-        /** Default destructor */
-        ~Node();
-        /** Copy Constructor **/
-        Node::Node(const Node& other);
+private:
+    // The number of children this can have.
+    const int NODE_CAPACITY = 4;
 
-        Node(Node* parent);
+    // Parent of the Node, if the node is empty, then the node is the head.
+    Node* Parent;
 
-        Node* findNode(long x, long y);
+    // The Quadrants beneath the tree.
+    Node* nw;
+    Node* ne;
+    Node* sw;
+    Node* se;
 
-        Node* findCell(long x, long y);
+    // The Data, if any, in the node.
+    Data data;
 
-        int* getData();
-
-        void setData(int data[5]);
-
-        int* getDataFromTile(long x, long y);
-
-        Node* destroyNode(Node* node);
-
-        void destroyQ1() {Q1=destroyNode(Q1);}
-
-        void destroyQ2() {Q2=destroyNode(Q2);}
-
-        void destroyQ3() {Q3=destroyNode(Q3);}
-
-        void destroyQ4() {Q4=destroyNode(Q4);}
-
-
-    private:
-        // The parent node of this node. If null then the node is the head.
-        Node* m_parent;
-        // The child nodes of this node. If null then the child does not exist. If all are null then this node is a leaf.
-        Node* Q1, *Q2, *Q3, *Q4;
-        // The coordinates of the node used for searching. Unless the height is 0 then this is actually a whole number, if it's not then it's the average of it's children
-        long m_x, m_y;
-        // The height of the node in the tree. 0 is a tile. 3 is a room. 6 is a Cell.
-        int m_height;
-        // The data (if any) in the tile. Has a slot for each thing that can occupy the room.
-        int m_data[5];
-};
+    // Constructors
+    Node();
+    Node(const Node& Parent);
+    
+    // Destructor
+    ~Node();
+}
 
 #endif // NODE_H

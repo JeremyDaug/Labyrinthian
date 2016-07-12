38993d76e0c3b38278aaf4e269402fa5762dca61
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2016-07-12 00:10:46 -0500

    The split of the Testing Branch. DO NOT PULL THIS TO MASTER.

---
+++ TestingMain.cpp
@@ -0,0 +1,8 @@
+// Testing file to double check Labyrinthian work without doing a full run of the program.
+
+#include<iostream>
+
+int main() {
+	std::cout << "Starting Tests... \n";
+	return 0;
+}

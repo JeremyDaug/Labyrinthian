1b73ce05286585563bda698fec4e60b074e7783c
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2016-07-12 20:55:36 -0500

    Updated testing functions to make it work. Congrats, you stopped breaking things.

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -1,6 +1,7 @@
 // Testing file to double check Labyrinthian work without doing a full run of the program.
 
 #include<iostream>
+#include "DunMap.h"
 
 int main() {
 	std::cout << "Starting Tests... \n";
